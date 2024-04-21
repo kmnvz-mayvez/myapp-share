05b7fc4cbf3ccb47de6d7a79147c3637ac44d5e4
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2024-04-21 17:43:26 +0700

    remove JWT_TOKEN

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -1,12 +1,8 @@
 import JWT from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from './error-handler';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const tokens: string[] = ['auth', 'seller', 'fish', 'search', 'buyer', 'message', 'order', 'review'];
-const JWT_TOKEN = process.env.JWT_SECRET as string;
 
 export function verifyGatewayRequest(req: Request, _res: Response, next: NextFunction): void {
   if (!req.headers?.gatewaytoken) {
@@ -18,7 +14,7 @@ export function verifyGatewayRequest(req: Request, _res: Response, next: NextFun
   }
 
   try {
-    const payload: { id: string; iat: number } = JWT.verify(token, JWT_TOKEN) as { id: string; iat: number };
+    const payload: { id: string; iat: number } = JWT.verify(token, '1282722b942e08c8a6cb033aa6ce850e') as { id: string; iat: number };
     if (!tokens.includes(payload.id)) {
       throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request payload is invalid');
     }
