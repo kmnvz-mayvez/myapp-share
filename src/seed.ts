653043767debede9420aafd8ab24a157c2c57e0d
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2024-04-26 16:47:09 +0700

    update seed

--- src/seed.ts
+++ src/seed.ts
@@ -1,4 +1,4 @@
-export const seedImage: string[] = [
+export const seedImage = [
     'https://res.cloudinary.com/diaej365u/image/upload/c_scale,w_400/v1713749566/lele_n0mwa0.jpg',
     'https://res.cloudinary.com/diaej365u/image/upload/c_scale,w_400/v1713750566/mas_tkuvxv.jpg',
     'https://res.cloudinary.com/diaej365u/image/upload/c_scale,w_400/v1713749745/patin_pfn49h.jpg',
